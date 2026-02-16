231faedf431375b55a0e1b362c09a120221e2e79
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-02-15 17:12:40 -0800

    feat: add test runner and rendering functionality
    - Implemented a Runner class to handle test execution and file collection.
    - Added render function to load HTML files using jsdom.
    - Created a .gitignore file to exclude node_modules and .DS_Store.
    - Updated package.json to include chalk and jsdom as dependencies.
    - Added sample project files with tests for forEach function and email validation.
    - Created HTML and JS files for a sample web project with form validation.

--- runner.js
+++ runner.js
@@ -2,10 +2,45 @@
 
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk').default;
+const render = require('./render');
+
+const forbiddenDirs = ['node_modules'];
 
 class Runner {
     constructor() {
         this.testFiles = [];
+    }
+
+    async runTests() {
+        for (let file of this.testFiles) {
+            console.log(chalk.grey(`----- ${file.shortName} -----`))
+            const beforeEaches = [];
+            global.render = render;
+            global.beforeEach = (fn) => {
+                beforeEaches.push(fn);
+            };
+            global.it = async (desc, fn) => {
+                beforeEaches.forEach(func => func());
+                try {
+                    await fn();
+                    console.log(chalk.green(`\tOK - ${desc}`));
+                } catch (err) {
+                    // sets error message to variable and globally replaces every new line with a new line + two indentation tabs
+                    const message = err.message.replace(/\n/g, '\n\t\t')
+                    console.log(chalk.red(`\tX - ${desc}`));
+                    console.log(chalk.red(`\t`, message));
+                }
+            };
+
+            // When we call require on 'file.name', which is a path to a test file, Node is going to load up all of the code inside of it and execute it.
+            try {
+                require(file.name);
+            } catch (err) {
+                console.log(chalk.green(`\tX - Error Loading File`, file.name));
+                console.log(chalk.red(`\t`, err.message));
+            }
+        }
     }
 
     async collectFiles(targetPath) {
@@ -18,8 +53,8 @@ class Runner {
             const stats = await fs.promises.lstat(filepath);
 
             if (stats.isFile() && file.includes('.test.js')) {
-                this.testFiles.push({ name: filepath })
-            } else if (stats.isDirectory()) {
+                this.testFiles.push({ name: filepath, shortName: file })
+            } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
                 const childFiles = await fs.promises.readdir(filepath);
                 
                 // since childFiles is also an array, we don't want to push it as is. Use ... syntax to pull out whatever is INSIDE the childFiles array and push those to files array.
